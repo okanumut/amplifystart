b867d4ae729e5aa556a7ebe54666e03089f25186
Okan Umut Acer <acer.okanumut@gmail.com>
2021-02-26 13:16:56 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,18 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import './App.css';
 
 function App() {
-  const [data, setData] = useState({ hits: [] });
-
-  useEffect(async () => {
-    const result = await axios(
-      'https://mfa30ao9w8.execute-api.us-east-1.amazonaws.com/dev/'
-    );
-
-    console.log(result);
-  });
-  return <div className='App'></div>;
+  return <div className='App'>Merhaba Amplify</div>;
 }
 
 export default App;
